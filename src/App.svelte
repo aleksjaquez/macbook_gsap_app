24c300750def9a26650a631094a1b3d40b89d1b7
aleksjaquez <aleksjaquez@gmail.com>
2025-10-19 14:52:30 -0600

    update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,13 @@
 <script>
-    import './app.css';
-    import NavBar from './components/NavBar.svelte';
-    import Hero from './components/Hero.svelte';
-    import PoductViewer from './components/PoductViewer.svelte';
+    import "./app.css";
+    import NavBar from "./components/NavBar.svelte";
+    import Hero from "./components/Hero.svelte";
+    import PoductViewer from "./components/PoductViewer.svelte";
 </script>
 
 <main>
     <NavBar />
     <Hero />
     <PoductViewer />
+    <h1>Prueba</h1>
 </main>
